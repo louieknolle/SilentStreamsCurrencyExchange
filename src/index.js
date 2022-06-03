9be22d98b6f2147a682157dbeaf3dc4bf4610db1
Louie Knolle <91914975+louieknolle@users.noreply.github.com>
2022-06-02 21:45:07 -0400

    add getMoneyConversion() and callExchangeApi()

--- src/index.js
+++ src/index.js
@@ -4,9 +4,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyService from './currency-service.js';
 
+function emptyText() {
+  $('#moneyValue').val('');
+  $('.showConvertedAmount').text('');
+}
 
+function roundToTwo(num) {
+  return +(Math.round(num + "e+2")  + "e-2");
+}
+
+function getMoneyConversion(response) {
+  if (response.result === 'success') {
+    $('.showConvertedAmount').text(`Your money is equal to ${roundToTwo(response.conversion_result)} ${response.target_code}.`)
+  } else {
+    $('.showConvertedAmount').text(`There was an error in your request: ${response.error-type}`);
+  }
+}
+
+async function callExchangeApi(initialCurrency, finalCurrency, moneyValue) {
+  const response = await CurrencyService.convertCurrency(initialCurrency, finalCurrency, moneyValue);
+  getMoneyConversion(response);
+}
 
 $(document).ready(function() {
-  //UI Logic goes here
+  $('#convertMoney').click(function() {
+    const initialCurrency = $('#currencyBegin').val();
+    const finalCurrency = $('#currencyEnd').val();
+    const moneyValue = $('#moneyValue').val();
+    emptyText();
+    callExchangeApi(initialCurrency, finalCurrency, moneyValue);
+  });
 });
 
